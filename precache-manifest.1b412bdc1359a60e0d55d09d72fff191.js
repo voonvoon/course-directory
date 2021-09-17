20823b056b21d6600b9ccd5015614e1759a4d2e5
voon220 <peter220@gmail.com>
2021-09-17 11:43:34 +0800

    Updates

---
+++ precache-manifest.1b412bdc1359a60e0d55d09d72fff191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e997a79d7122b264fded",
+    "url": "/course-directory/static/css/main.8f1193c5.chunk.css"
+  },
+  {
+    "revision": "e997a79d7122b264fded",
+    "url": "/course-directory/static/js/main.e997a79d.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "7a13309f73565a68d2d4e9bba38edbd9",
+    "url": "/course-directory/index.html"
+  }
+];
